19352d6ccf7173acdafc30c30b574cae8e11bdbf
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2025-12-18 00:28:16 +0530

    SPI UVM: add interface definition with master/slave modports

--- env/interface.sv
+++ env/interface.sv
@@ -9,19 +9,20 @@
 
 interface spi_interface;
     logic SCL;
-    logic resetn;
+    logic rst_n;
     logic CS_n;
     logic MOSI;
     logic MISO;
+    logic rx_valid;
     logic [7:0] tx_data;
     logic [7:0] rx_data;
 
-    modport master  ( output SCL, MOSI, tx_data, CS_n, 
-                     input MISO, rx_data
+    modport master  ( output SCL, CS_n, MOSI, tx_data, 
+                     input rx_data, MISO, rx_valid
                     ); 
 
-    modport slave   ( input SCL, MOSI, CS_n, tx_data,
-                    output MISO, rx_data
+    modport slave   ( input rst_n,SCL, CS_n, MOSI, tx_data,
+                    output rx_data, MISO, rx_valid
                     );
 
 endinterface
