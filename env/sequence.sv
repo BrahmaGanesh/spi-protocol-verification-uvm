ea0749c75648954bfc2a0ff846e49176e227b24a
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2025-12-18 00:30:04 +0530

    SPI UVM: add sequence class to generate transaction streams

---
+++ env/sequence.sv
@@ -0,0 +1,22 @@
+//=====================================================
+// Project     : SPI (Serial Peripheral Interface)
+// File        : sequence.sv
+// Author      : Brahma Ganesh Katrapalli
+// Created On  : 17-12-2025
+// Revision    : 1.0
+// Description : Sequence class generating transaction streams
+//=====================================================
+
+class spi_sequence extends uvm_sequence#(spi_transaction);
+    `uvm_object_utils(spi_sequence)
+  	
+    spi_transaction tr;
+  	
+    function new (string name="spi_sequence");
+        super.new(name);
+    endfunction
+  
+    virtual task body();
+    endtask
+  
+endclass
